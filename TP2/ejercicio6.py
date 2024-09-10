718a450036418a48da4c2068a5625d99d9917cde
Clari20 <126616185+Clari20@users.noreply.github.com>
2024-09-10 14:50:51 -0300

    Delete TP2 directory

--- TP2/ejercicio6.py
+++
@@ -1,9 +0,0 @@
-"""De la siguiente cadena “La lluvia en Mendoza es escasa” indique cual es el tamaño
-de la cadena es decir su número de caracteres."""
-
-
-cadena = str("La lluvia en Mendoza es escasa")
-
-long = len(cadena)
-
-print(f"La longitud de la cadena ''{cadena}'' es: {long} caracteres")
