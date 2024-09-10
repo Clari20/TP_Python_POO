e3e22ec9becd441090edc2af6207888b0e2ae45b
Clari20 <claritamitre@gmail.com>
2024-09-10 15:09:53 -0300

    Ejercicio4

---
+++ TP3/ejercicio4.py
@@ -0,0 +1,27 @@
+"""Escribe un programa que pida al usuario una lista de números y cuente cuántos de
+ellos son pares y cuántos son impares."""
+
+
+entrada = input("Ingrese una lista de números separados por espacios: ")
+
+
+numeros = list(map(int, entrada.split()))
+
+par = 0
+impar = 0
+
+# Contar pares e impares
+for numero in numeros:
+    if numero % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+
+# Mostrar el resultado
+print(f"Números pares: {par}")
+print(f"Números impares: {impar}")
+
+
+
+
+
