95e8a00b4dc988d763fc7af321174f8165478c79
Clari20 <claritamitre@gmail.com>
2024-10-03 18:09:35 -0300

    Ejercicio 1 2 3 4 5

---
+++ TP4/ejercicio1.py
@@ -0,0 +1,13 @@
+"""Dada la siguiente tupla: punto = (10, 20) 
+a) Desempaqueta los valores de la tupla en las variables x y y. 
+b) Imprime el valor de cada variable. """
+
+punto = (10, 20) 
+
+x = punto[0]
+
+y = punto[1]
+
+print(x)
+
+print(y)
---
+++ TP4/ejercicio3.py
@@ -0,0 +1,12 @@
+"""Dada la siguiente tupla: frutas = ("manzana", "banana", "cereza", "naranja", "kiwi") 
+a) Imprime el segundo elemento de la tupla. 
+b) Imprime los tres primeros elementos de la tupla. 
+c) Imprime el último elemento de la tupla usando un índice negativo."""
+
+frutas = ("manzana", "banana", "cereza", "naranja", "kiwi")
+
+print(frutas[1])
+
+print(frutas[0], frutas[1], frutas[2])
+
+print(frutas[-1])
